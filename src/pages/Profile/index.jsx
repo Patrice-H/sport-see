eba94b558cba53710148f3f7e256ab957a50d28c
Patrice-H <webmestre64@gmail.com>
2022-06-01 22:33:42 +0200

    Add navbars and get data

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import HorizontalNavbar from '../../components/HorizontalNavbar';
+import VerticalNavbar from '../../components/VerticalNavbar';
+import { getUserInformations } from '../../services/callsAPI';
+import './Profile.css';
+
 const Profile = () => {
-  return <h1>Profil</h1>;
+  const param = useParams();
+  const [profileData, setProfileData] = useState();
+
+  useEffect(() => {
+    getUserInformations(parseInt(param.profileId)).then((res) => {
+      setProfileData(res.data.data);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <VerticalNavbar />
+      <HorizontalNavbar />
+      <h1 className="profile-title">
+        <span>Bonjour </span>
+        <span className="profile-name">
+          {profileData && profileData.userInfos.firstName}
+        </span>
+      </h1>
+    </>
+  );
 };
 
 export default Profile;
