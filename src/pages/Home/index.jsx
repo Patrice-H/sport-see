5e49369bc50f5639ecfea90f47520c4038c3a38e
Patrice-H <webmestre64@gmail.com>
2022-06-01 22:28:13 +0200

    Add links to profile page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import VerticalNavbar from '../../components/VerticalNavbar';
+import HorizontalNavbar from '../../components/HorizontalNavbar';
+import './Home.css';
+
 const Home = () => {
-  return <h1>Accueil</h1>;
+  return (
+    <>
+      <VerticalNavbar />
+      <HorizontalNavbar />
+      <div className="home-content">
+        <h1>Accueil</h1>
+        <Link to="/profile/12">Utilisateur 12</Link>
+        <br />
+        <Link to="/profile/18">Utilisateur 18</Link>
+      </div>
+    </>
+  );
 };
 
 export default Home;
