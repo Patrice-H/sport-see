16443f067dce816016193d1f1220a732e9d49366
Patrice-H <webmestre64@gmail.com>
2022-06-07 09:55:02 +0200

    Add a service to call Api and get activity data

--- src/services/callsAPI.js
+++ src/services/callsAPI.js
@@ -12,3 +12,16 @@ export const getUserInformations = (userId) => {
       .catch((error) => console.log(error))
   );
 };
+
+export const getUserActivity = (userId) => {
+  return (
+    axios
+      .get(`http://localhost:3000/user/${userId}/activity`)
+      /*
+    .then((response) => {
+      console.log(response.data.data.sessions);
+      return response.data;
+    })*/
+      .catch((error) => console.log(error))
+  );
+};
