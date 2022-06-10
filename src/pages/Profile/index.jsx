6d9749af08fc5baa0a887789415a8500576ea2b4
Patrice-H <webmestre64@gmail.com>
2022-06-10 23:22:55 +0200

    Update profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,51 +1,39 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-//import ActivityBarChart from '../../components/ActivityBarChart';
 import ActivityChart from '../../components/ActivityChart';
-//import CustomComponent from '../../components/CustomComponent';
 import HorizontalNavbar from '../../components/HorizontalNavbar';
+import SessionsLineChart from '../../components/SessionsLineChart';
 import VerticalNavbar from '../../components/VerticalNavbar';
-import { getUserInformations, getUserActivity } from '../../services/callsAPI';
+import {
+  getUserInformations,
+  getUserActivity,
+  getUserSessions,
+} from '../../services/callsAPI';
 import './Profile.css';
 
 const Profile = () => {
   const param = useParams();
   const [profileData, setProfileData] = useState();
   const [userActivity, setUserActivity] = useState();
+  const [userSessions, setUserSessions] = useState();
 
   useEffect(() => {
     getUserInformations(parseInt(param.profileId)).then((res) => {
       setProfileData(res.data.data);
     });
+
     getUserActivity(parseInt(param.profileId)).then((res) => {
       setUserActivity(res.data.data.sessions);
+    });
+
+    getUserSessions(parseInt(param.profileId)).then((res) => {
+      setUserSessions(res.data.data.sessions);
       console.log(res.data.data.sessions);
     });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  /*
-  const [dimensions, setDimensions] = useState({
-    height: window.innerHeight,
-
-    width: window.innerWidth,
-  });
-
-  useEffect(() => {
-    function handleResize() {
-      setDimensions({
-        height: window.innerHeight,
-
-        width: window.innerWidth,
-      });
-    }
-    window.addEventListener('resize', handleResize);
-  }, [dimensions]);
-
-  console.log(dimensions.width + ' x ' + dimensions.height);
-  */
-  // <ActivityChart data={userActivity.sessions} />
   return (
     <div>
       <VerticalNavbar />
@@ -56,9 +44,15 @@ const Profile = () => {
           {profileData && profileData.userInfos.firstName}
         </span>
       </h1>
-      <div className="activity-chart-container">
-        <p className="activity-chart-title">Activité quotidienne</p>
-        <ActivityChart data={userActivity} />
+      <div className="all-charts-container">
+        <div className="activity-chart">
+          <ActivityChart data={userActivity} />
+        </div>
+        <div className="triple-charts">
+          <div className="sessions-chart">
+            <SessionsLineChart data={userSessions} />
+          </div>
+        </div>
       </div>
     </div>
   );
