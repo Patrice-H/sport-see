8fc85b5700fac98ccd427b3dc5ce67397413f4b3
Patrice-H <webmestre64@gmail.com>
2022-06-10 23:21:16 +0200

    Add a service to call Api and get sessions data

--- src/services/callsAPI.js
+++ src/services/callsAPI.js
@@ -1,27 +1,19 @@
 import axios from 'axios';
 
 export const getUserInformations = (userId) => {
-  return (
-    axios
-      .get(`http://localhost:3000/user/${userId}`)
-      /*
-    .then((response) => {
-      console.log(response.data.data);
-      return response.data;
-    })*/
-      .catch((error) => console.log(error))
-  );
+  return axios
+    .get(`http://localhost:3000/user/${userId}`)
+    .catch((error) => console.log(error));
 };
 
 export const getUserActivity = (userId) => {
-  return (
-    axios
-      .get(`http://localhost:3000/user/${userId}/activity`)
-      /*
-    .then((response) => {
-      console.log(response.data.data.sessions);
-      return response.data;
-    })*/
-      .catch((error) => console.log(error))
-  );
+  return axios
+    .get(`http://localhost:3000/user/${userId}/activity`)
+    .catch((error) => console.log(error));
+};
+
+export const getUserSessions = (userId) => {
+  return axios
+    .get(`http://localhost:3000/user/${userId}/average-sessions`)
+    .catch((error) => console.log(error));
 };
