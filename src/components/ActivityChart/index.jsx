4973d33c119a2c064d13b5bef17e69018092db31
Patrice-H <webmestre64@gmail.com>
2022-06-10 23:15:49 +0200

    Update ActivityChart component

--- src/components/ActivityChart/index.jsx
+++ src/components/ActivityChart/index.jsx
@@ -19,68 +19,80 @@ const CustomTooltip = ({ active, payload }) => {
       </div>
     );
   }
+
+  return null;
 };
 
 const xAxisFormatter = (tickItem) => {
   if (tickItem.length > 4) return parseInt(tickItem.split('-')[2]);
 };
 
+const ActivityChartTitle = () => {
+  return <p className="activity-chart-title">Activité quotidienne</p>;
+};
+
 const ActivityChart = ({ data }) => {
   return (
-    <ResponsiveContainer>
-      <BarChart data={data} margin={{ top: 5, right: 0, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="2 2" vertical={false} />
-        <XAxis
-          dataKey="day"
-          tickFormatter={xAxisFormatter}
-          tickLine={false}
-          dy={10}
-        />
-        <YAxis
-          dataKey="kilogram"
-          orientation="right"
-          tickCount={3}
-          domain={['dataMin - 1', 'auto']}
-          yAxisId="right"
-          axisLine={false}
-          tickLine={false}
-        />
-        <YAxis
-          dataKey="calories"
-          orientation="left"
-          yAxisId="left"
-          hide={true}
-        />
-        <Tooltip
-          content={<CustomTooltip />}
-          contentStyle={{ backgroundColor: 'red' }}
-          labelStyle={{ display: 'none' }}
-          itemStyle={{ color: 'white' }}
-        />
-        <Legend
-          align="right"
-          verticalAlign="top"
-          iconType="circle"
-          wrapperStyle={{ top: -20 }}
-        />
-        <Bar
-          dataKey="kilogram"
-          name="Poids (Kg)"
-          fill="#282d30"
-          radius={4}
-          barSize={8}
-          yAxisId="right"
-        />
-        <Bar
-          dataKey="calories"
-          name="Calories brûlées (kCal)"
-          fill="#ff0000"
-          radius={4}
-          barSize={8}
-          yAxisId="left"
-        />
-      </BarChart>
-    </ResponsiveContainer>
+    <article className="activity-chart-container">
+      <ActivityChartTitle />
+      <ResponsiveContainer>
+        <BarChart
+          data={data}
+          margin={{ top: 5, right: 0, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="2 2" vertical={false} />
+          <XAxis
+            dataKey="day"
+            tickFormatter={xAxisFormatter}
+            tickLine={false}
+            dy={10}
+          />
+          <YAxis
+            dataKey="kilogram"
+            orientation="right"
+            tickCount={3}
+            domain={['dataMin - 1', 'auto']}
+            yAxisId="right"
+            axisLine={false}
+            tickLine={false}
+          />
+          <YAxis
+            dataKey="calories"
+            orientation="left"
+            yAxisId="left"
+            hide={true}
+          />
+          <Tooltip
+            content={<CustomTooltip />}
+            contentStyle={{ backgroundColor: 'red' }}
+            labelStyle={{ display: 'none' }}
+            itemStyle={{ color: 'white' }}
+          />
+          <Legend
+            align="right"
+            verticalAlign="top"
+            iconType="circle"
+            wrapperStyle={{ top: -20 }}
+          />
+          <Bar
+            dataKey="kilogram"
+            name="Poids (Kg)"
+            fill="#282d30"
+            radius={4}
+            barSize={8}
+            yAxisId="right"
+          />
+          <Bar
+            dataKey="calories"
+            name="Calories brûlées (kCal)"
+            fill="#ff0000"
+            radius={4}
+            barSize={8}
+            yAxisId="left"
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </article>
   );
 };
 
