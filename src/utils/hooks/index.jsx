80c4debf2be21f2c49deead44de9350d7ca8c651
Patrice-H <webmestre64@gmail.com>
2022-06-20 14:20:18 +0200

    Manage redirection to error page and add jsdoc

--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -1,28 +1,34 @@
 import { useState, useEffect } from 'react';
-//import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { dataSRC } from '../config';
 
+/**
+ * Custom hook that fetches data from a URL and returns the data.
+ * If data are not found redirect to error page
+ *
+ * @returns An object with data
+ */
 export const useFetch = (url) => {
   const [data, setData] = useState();
-  const [isDataLoading, setDataLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (!url) return;
-    setDataLoading(true);
+    if (!url || dataSRC !== 'API') return;
     fetch(url)
-      .then((response) =>
-        response.json().then((data) => {
-          setData(data);
-          setDataLoading(false);
-          //console.log(data.data.userInfos);
-        })
-      )
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((data) => setData(data));
+        } else {
+          navigate('/error404');
+        }
+      })
       .catch((error) => {
+        navigate('/error500');
         console.log(error);
-        setError(true);
-        setDataLoading(false);
       });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [url]);
 
-  return { data, isDataLoading, error };
+  return { data };
 };
